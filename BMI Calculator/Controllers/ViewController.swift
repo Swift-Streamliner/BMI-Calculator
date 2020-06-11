41f9438c68cb266ca445c916e18ef8a43517b13b
Tiger <thisisstreamliner@gmail.com>
2020-06-11 17:39:18 +0300

    init project and setup weight and height sliders

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onHeightChanged(_ sender: UISlider) {
+        let value = String(format: "%.2f", sender.value)
+        print("height \(value)")
+        heightLabel.text = "\(value)m"
+    }
+    @IBAction func onWeightChanged(_ sender: UISlider) {
+        print("weight \(Int(sender.value))")
+        weightLabel.text = "\(String(Int(sender.value)))Kg"
+    }
+    
 }
 
